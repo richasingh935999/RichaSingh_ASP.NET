806a2f5938353712a35e0accfda50a387abf4d79
richasingh935999 <73345834+richasingh935999@users.noreply.github.com>
2022-07-27 15:25:12 +0530

    Add files via upload

---
+++ Day3/Repositries/IEmployeeRepo.cs
@@ -0,0 +1,14 @@
+﻿using WebApplication5.Models;
+
+namespace WebApplication5.Repositries
+{
+    
+        public interface IEmployeeRepo
+        {
+            List<Employee> GetAllEmployees();
+            Employee GetEmployeeById(int id);
+            void AddEmployee(Employee obj);
+            void UpdateEmployee(Employee obj);
+            void DeleteEmployee(int id);
+        }
+    }
